fbda3c8a6117d984db59871d1edc3573e321b329
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-28 19:56:56 -0600

    Create function to print grid and update main()

--- multigrid.py
+++ multigrid.py
@@ -1,5 +1,5 @@
 def main():
-    ...
+    printSquare(getSquare())
 
 def getSquare():
     try:
@@ -7,6 +7,18 @@ def getSquare():
         return dimension
     except ValueError:
         print('Enter a valid integer')
+
+
+#LOGIC 1
+def printSquare(dim):
+    try:
+        for i in range(dim):
+            for j in range(dim):
+                print('#', end='')
+            print()
+    except TypeError:
+        print('Range() needs a valid integer')
+
 
 if __name__ == '__main__':
     main()
