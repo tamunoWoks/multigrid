c0f39cf7cd03b21ddfe006dfd8d728f47d521c8f
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-28 19:52:30 -0600

    Create getSquare() to get dimension of square

--- multigrid.py
+++ multigrid.py
@@ -1,5 +1,12 @@
 def main():
     ...
 
+def getSquare():
+    try:
+        dimension = int(input('Enter dimension of square: '))
+        return dimension
+    except ValueError:
+        print('Enter a valid integer')
+
 if __name__ == '__main__':
     main()
